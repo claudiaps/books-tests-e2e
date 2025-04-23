26e7eeb96675d50eac4dbe0067bfdbf81288fbfd
claudiaps <clp.sampedro@gmail.com>
2025-04-22 21:07:51 -0300

    test: adicionando mais cenários de teste

--- tests/books-search.spec.ts
+++ tests/books-search.spec.ts
@@ -6,6 +6,16 @@ import { test, expect } from "@playwright/test";
 test("Search books and render as a list", async ({ page }) => {
     await page.goto("http://localhost:5173/books")
     const title = await page.getByText("Busca de Livros")
-    console.log(title)
+
     expect(title).toBeTruthy()
+
+    await page.getByRole("textbox").fill("Harry Potter")
+    await page.getByRole("button", { name: "Buscar" }).click()
+    await page.waitForSelector("ul > li")
+
+    const listCount = await page.locator("ul > li").count()
+    expect(listCount).toBeGreaterThan(0)
+
+    const firstItem = await page.locator("ul > li").first()
+    expect(firstItem).toContainText(/Harry Potter/)
 })
